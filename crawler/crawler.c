364245b7fc8b6c9e5182c1d12c8a0d3a3d1904a4
Samuel Hirsh <hirshsamuel48@gmail.com>
2024-10-21 16:12:58 -0400

    Step 4 done

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -15,6 +15,7 @@
 #include <webpage.h>
 #include <string.h>
 #include <queue.h>
+#include <hash.h>
 
 void printURL(webpage_t *wp) {
 	char* urlp = webpage_getURL(wp);
@@ -22,6 +23,11 @@ void printURL(webpage_t *wp) {
 
 	printf("Depth: %d, url: %s \n", depth, urlp);
 	free(urlp);
+}
+
+bool compareURL(webpage_t *wp, char* url) {
+	char* urlp = webpage_getURL(wp);
+	return strcmp(urlp, url) == 0;
 }
 
 
@@ -50,16 +56,21 @@ int  main() {
 	char *result;
 
 	queue_t *wp_qp = qopen();
-
+	hashtable_t *wp_hp = hopen(100);
+	
 	while((pos = webpage_getNextURL(homepage_p, pos, &result)) > 0) {
 		if (IsInternalURL(result)) {
 
 			webpage_t *wp = webpage_new(result, webpage_getDepth(homepage_p) + 1, NULL);
-			qput(wp_qp, wp);
-			
-			printf("Found internal URL: ");
+
+			if (hsearch(wp_hp, (bool (*)(void*, void*))compareURL, result, strlen(result)) == NULL) {
+					qput(wp_qp, wp);
+					hput(wp_hp, wp, result, strlen(result));
+			}
+				
+			printf("Found internal url: ");
 		} else {
-			printf("Found external URL: ");
+			printf("Found external url: ");
 		}
 		
 		printf("%s\n", result);
@@ -67,12 +78,13 @@ int  main() {
 	}
 	
 	printf("\n");
-	printf("URLs in queue: \n");
+	printf("urls in queue: \n");
 
 	// Print all the URLs in the Queue
 	qapply(wp_qp, (void (*)(void *))printURL); 
 	qclose(wp_qp);
-
+	hclose(wp_hp);
+	
 	free(homepage_p);
 	free(html);
 
