8ac4b5a0d328e9a781b0e050adb7becbd2aa0da4
Samuel Hirsh <hirshsamuel48@gmail.com>
2024-10-21 11:34:45 -0400

    Makefile

---
+++ crawler/crawler.c
@@ -0,0 +1,18 @@
+/* 
+ * crawler.c --- 
+ * 
+ * Author: Samuel R. Hirsh
+ * Created: 10-21-2024
+ * Version: 1.0
+ * 
+ * Description: 
+ * 
+ */
+
+
+#include <stdio.h>
+#include <webpage.h>
+
+int  main() {
+	printf("Hello");
+}
