83ea65060f987b84f7753db511c04256133a9f72
Samuel Hirsh <hirshsamuel48@gmail.com>
2024-10-21 12:19:38 -0400

    Module 4 step 2

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -12,7 +12,37 @@
 
 #include <stdio.h>
 #include <webpage.h>
+#include <string.h>
 
 int  main() {
-	printf("Hello");
+	char url[] = "https://thayer.github.io/engs50/"; 
+	const int depth = 0;
+	
+	webpage_t* webpage = webpage_new(url, depth, NULL);
+	webpage_fetch(webpage);
+	char* html = webpage_getHTML(webpage);
+	
+	if (webpage != NULL) {
+		printf("[+] Successfully got webpage\n");
+		printf("%s", html);
+	} else {
+		exit(EXIT_FAILURE);
+	}
+
+	int pos = 0;
+	char *result;
+
+	while((pos = webpage_getNextURL(webpage, pos, &result)) > 0) {
+		if (IsInternalURL(result)) {
+			printf("Found internal URL: ");
+		} else {
+			printf("Found external URL: ");
+		}
+		
+		printf("%s\n", result);
+	}
+
+	free(result);
+	free(webpage);
+	free(html);
 }
