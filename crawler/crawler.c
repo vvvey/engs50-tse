21f2db56c645776c1d56f360d2a929e57ac4ecc0
vvvey <vuthyvey25@gmail.com>
2024-10-22 23:05:35 -0400

    Finished Step 6

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -12,10 +12,14 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-#include <webpage.h>
+#include <sys/stat.h>
 #include <string.h>
+
+// Custom headers
+#include <webpage.h>
 #include <queue.h>
 #include <hash.h>
+
 
 void printURL(webpage_t *wp) {
 	char* urlp = webpage_getURL(wp);
@@ -46,67 +50,94 @@ int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
 	
 	fprintf(fl, "%s\n%d\n%d\n%s", urlp, depth, len, html);
 	fclose(fl);
+	return 0;
 }
 
 
-int  main() {
-	char url[] = "https://thayer.github.io/engs50/"; 
-	const int depth = 0;
+int  main(int argc, char *argv[]) {
+	// Check arguments
+	if (argc != 4) {
+		printf("Usage: crawler <seedurl> <pagedir> <maxdepth> \n ");
+		exit(EXIT_FAILURE);
+	}
+
+	char *ep; 
+	char *seedurl = argv[1];
+	char *pagedir = argv[2];
+	int maxdepth = strtod(argv[3], &ep);
 	
-	webpage_t* homepage_p = webpage_new(url, depth, NULL);
+	printf("%s %s %d \n", seedurl, pagedir, maxdepth);
 
-	if (homepage_p == NULL) { // Can't open a webpage
-		free(homepage_p);
+	// Check if page dir exists
+	struct stat sb;
+
+    if (stat(pagedir, &sb) != 0) {
+		printf("%s directory doesn't exist \n", pagedir);
+        printf("Usage: crawler <seedurl> <pagedir> <maxdepth> \n ");
+		exit(EXIT_FAILURE);
+    } 
+
+	// check if maxdepth is positive
+	if (strcmp(ep, "") != 0 || maxdepth < 0) {
+		printf("Usage: crawler <seedurl> <pagedir> <maxdepth> \n ");
 		exit(EXIT_FAILURE);
 	}
 
-	bool isFetch = webpage_fetch(homepage_p);
-	if (isFetch == false) { // Can't fetch page
-		free(homepage_p);
-		exit(EXIT_FAILURE);
-	}
-
-	pagesave(homepage_p, 1, "pages");
-
-	char* html = webpage_getHTML(homepage_p);
-	//printf("[+] Successfully got webpage\n");
-	//printf("%s", html);
-
-	int pos = 0;
-	char *result;
+	//char url[] = "https://thayer.github.io/engs50/"; 
+	int depth = 0;
+	webpage_t* homepage_p = webpage_new(seedurl, depth, NULL);
 
 	queue_t *wp_qp = qopen();
 	hashtable_t *wp_hp = hopen(100);
+
+	// Add homepage to queue and hash
+	qput(wp_qp, homepage_p);
+	hput(wp_hp, homepage_p, seedurl, strlen(seedurl));
 	
-	while((pos = webpage_getNextURL(homepage_p, pos, &result)) > 0) {
-		if (IsInternalURL(result)) {
+	webpage_t *currp = (webpage_t*)qget(wp_qp);
+	int file_id = 1;
+	
+	while (currp != NULL) {
 
-			webpage_t *wp = webpage_new(result, webpage_getDepth(homepage_p) + 1, NULL);
-
-			if (hsearch(wp_hp, (bool (*)(void*, void*))compareURL, result, strlen(result)) == NULL) {
-					qput(wp_qp, wp);
-					hput(wp_hp, wp, result, strlen(result));
-			}
-				
-			printf("Found internal url: ");
-		} else {
-			printf("Found external url: ");
+		bool isFetch = webpage_fetch(currp);
+		if (isFetch == false) { // Can't fetch page
+			printf("Can't fetch: %s \n", webpage_getURL(currp));
+			currp = qget(wp_qp);
+			continue;
 		}
-		
-		printf("%s\n", result);
-		free(result);
-	}
-	
-	printf("\n");
-	printf("urls in queue: \n");
 
-	// Print all the URLs in the Queue
-	qapply(wp_qp, (void (*)(void *))printURL); 
+		// Save page
+		printf("Saving: %s \n", webpage_getURL(currp));
+		pagesave(currp, file_id, pagedir);
+
+		int currdepth = webpage_getDepth(currp);
+
+		int pos = 0;
+		char *result;
+		
+		while(currdepth < maxdepth && (pos = webpage_getNextURL(currp, pos, &result)) > 0) {
+			if (IsInternalURL(result)) {
+
+				webpage_t *wp = webpage_new(result, currdepth + 1, NULL);
+
+				if (hsearch(wp_hp, (bool (*)(void*, const void*))compareURL, result, strlen(result)) == NULL) {
+						qput(wp_qp, wp);
+						hput(wp_hp, wp, result, strlen(result));
+				}
+			} 
+
+			free(result);
+		}	
+
+		file_id = file_id + 1;
+		currp = qget(wp_qp);
+	}
+	free(currp);
+	
 	qclose(wp_qp);
 	hclose(wp_hp);
 	
 	free(homepage_p);
-	free(html);
 
 	exit(EXIT_SUCCESS);
 }
