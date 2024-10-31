6f0d3fdeae9a7fc236b592075bd9b037358ac2a0
vvvey <vuthyvey25@gmail.com>
2024-10-31 13:01:27 -0400

    Fixed PageLoad Bug

--- test/test_pageio1.c
+++ test/test_pageio1.c
@@ -1,7 +1,7 @@
 /* 
  * pageio.c --- 
  * 
- * Author: 
+ * Author: Vuthy Vey
  * Created: 10-24-2024
  * Version: 1.0
  * 
@@ -38,11 +38,11 @@ int main() {
 
     pagesave(hp, 1, "pages");
     webpage_t *new_hp = pageload(1, "pages");
-
-    if (new_hp == NULL || strcmp(webpage_getHTML(hp), webpage_getHTML(new_hp)) != 0 || strcmp(webpage_getURL(hp), webpage_getURL(new_hp)) != 0 || webpage_getDepth(hp) == webpage_getDepth(new_hp)) {
+   
+    if (new_hp == NULL || strcmp(webpage_getHTML(hp), webpage_getHTML(new_hp)) != 0 || strcmp(webpage_getURL(hp), webpage_getURL(new_hp)) != 0 || webpage_getDepth(hp) != webpage_getDepth(new_hp)) {
         webpage_delete(hp);
         webpage_delete(new_hp);
-        
+
         exit(EXIT_FAILURE);
     } else {
         webpage_delete(hp);
--- utils/pageio.c
+++ utils/pageio.c
@@ -1,7 +1,7 @@
 /* 
  * pageio.c --- 
  * 
- * Author: 
+ * Author: Sam Hirsh & Vuthy Vey
  * Created: 10-24-2024
  * Version: 1.0
  * 
@@ -18,7 +18,7 @@ int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
 	char* urlp = webpage_getURL(pagep);
 	int depth = webpage_getDepth(pagep);
 	char* html = webpage_getHTML(pagep);
-	int len = strlen(html);
+	int len = webpage_getHTMLlen(pagep);
 
 	char fp[300];
 	sprintf(fp, "%s/%d", dirname, id);
@@ -42,27 +42,28 @@ webpage_t *pageload(int id, char *dirnm) {
   if (!fl) return NULL;
 
   char url[1024];
-
-	if (fscanf(fl, "%1023s\n", url) != 1) {
-    fclose(fl);
-    return NULL;
-  }
-
   int depth;
   int html_len;
 
-	if (fscanf(fl, "%d\n%d\n", &depth, &html_len) != 2) {
+  if (fscanf(fl, "%[^\n]\n%d\n%d\n", url, &depth, &html_len) != 3) {
     fclose(fl);
     return NULL;
   }
 
-	char line[1024];
-	char html[html_len * 1024];
+  char *html = malloc(html_len + 1); 
+  if (!html) {
+	fclose(fl);
+    return NULL; 
+  }
+
+  for (int index = 0; index < html_len; index = index + 1) {
+	int ch = fgetc(fl);
 	
-	while (fscanf(fl,"%1023s\n", line) != 1) {
-		strcat(html, line);
-	}
-	
+	html[index] = (char)ch;
+  }
+	html[html_len] = '\0';
+	fclose(fl);
+  
   webpage_t *page = webpage_new(url, depth, html);
 
 	if (!page) {
