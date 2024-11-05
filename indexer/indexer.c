8c2970dd12318ee6fec75c50cc84ca1e720db399
Samuel Hirsh <hirshsamuel48@gmail.com>
2024-11-04 22:55:40 -0500

    5 and 7

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -8,77 +8,128 @@
  * Description: 
  * 
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
 #include <pageio.h>
 #include <webpage.h>
 #include <hash.h>
+#include <queue.h>
+#include <indexio.h>
 
 int total_count = 0;
 
 typedef struct {
-    int count;
-    char *word; // normalized word
+  queue_t *doc_queue; // queue of documents containing the word
+  char *word; // normalized word
 } index_t;
 
-bool compareWord(index_t *ip, char* word) {
-	return strcmp(ip->word, word) == 0;
+typedef struct {
+  int doc_id; // document ID
+  int count; // word count in the document
+} doc_t;
+
+bool compareWord(index_t *ip, char *word) {
+  return strcmp(ip->word, word) == 0;
+}
+
+bool compareDocID(doc_t *dp, int *doc_id) {
+  return dp->doc_id == *doc_id;
 }
 
 void countTotal(index_t *ip) {
-    total_count += ip->count;
+  queue_t *doc_queue = ip->doc_queue;
+  doc_t *docp;
+
+  while ((docp = qget(doc_queue)) != NULL) {
+    total_count += docp->count;
+    free(docp);
+  }
 }
 
 int NormalizeWord(char *sp) {
-    if (strlen(sp) < 4) { 
-        return -1; // return Failure
-    }
-    for (int i = 0; sp[i] != '\0'; i++) {
-        if (sp[i] < 65 || sp[i] > 122 || (sp[i] > 90 && sp[i] < 97)) { // Non alphabet
-            return -1;
-        }
+  if (strlen(sp) < 4) {
+    return -1;
+  }
 
-        sp[i] = tolower(sp[i]);
+  for (int i = 0; sp[i] != '\0'; i++) {
+    if (sp[i] < 65 || sp[i] > 122 || (sp[i] > 90 && sp[i] < 97)) {
+      return -1;
     }
-    return 0;
+    sp[i] = tolower(sp[i]);
+  }
+  return 0;
 }
 
-int main() {
+hashtable_t* index_documents(int end_id, char *page_dir) {
+  hashtable_t *index_p = hopen(100);
 
-    // webpage_t *wp = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
-    // webpage_fetch(wp);
+  for (int doc_id = 1; doc_id <= end_id; doc_id++) {
+    webpage_t *wp = pageload(doc_id, page_dir);
 
-    webpage_t *wp = pageload(1, "../crawler/pages");
-    hashtable_t *index_p = hopen(100);
-
+		if (!wp) {
+			printf("Page could not load");
+			continue;
+		}
 
     char *word;
     int pos = 0;
+
     while ((pos = webpage_getNextWord(wp, pos, &word)) > 0) {
+      if (NormalizeWord(word) == 0) {
+        index_t *elementp = hsearch(index_p, (bool (*)(void*, const void*))compareWord, word, strlen(word));
 
-        if (NormalizeWord(word) == 0) {
-            printf("%s \n", word);
+        if (elementp == NULL) {
+          index_t *ip = (index_t*)malloc(sizeof(index_t));
+          ip->doc_queue = qopen();
+          ip->word = word;
 
-            index_t *elementp = hsearch(index_p, (bool (*)(void*, const void*))compareWord, word, strlen(word));
-            if (elementp == NULL) { // Word doesn't exist in hash table
-                index_t *ip = (index_t*)malloc(sizeof(index_t));
-                ip->count = 1;
-                ip->word = word;
-                hput(index_p, ip, word, strlen(word)); 
-			} else {
-                elementp->count = (elementp->count)+1; // Word exist in hash table so increment count by 1
-                free(word);
-			}
-        }   
+          doc_t *docp = (doc_t*)malloc(sizeof(doc_t));
+          docp->doc_id = doc_id;
+          docp->count = 1;
+          qput(ip->doc_queue, docp);
+
+          hput(index_p, ip, word, strlen(word));
+        } else {
+          doc_t *docp = qsearch(elementp->doc_queue, (bool (*)(void*, const void*))compareDocID, &doc_id);
+
+          if (docp == NULL) {
+            doc_t *new_docp = (doc_t*)malloc(sizeof(doc_t));
+            new_docp->doc_id = doc_id;
+            new_docp->count = 1;
+            qput(elementp->doc_queue, new_docp);
+          } else {
+            docp->count++;
+          }
+
+					free(word);
+        }
+      }
     }
 
-    happly(index_p, (void (*)(void *))countTotal );
-    printf("Total %d \n", total_count);
-    
-    
-    webpage_delete(wp);
-    exit(EXIT_SUCCESS);
+		webpage_delete(wp);
+  }
+
+  happly(index_p, (void (*)(void *))countTotal);
+
+  printf("Total %d \n", total_count);
+  
+  return index_p;
+}
+
+int main(int argc, char *argv[]) {
+	char *pgdir = argv[1];
+	char *idfl = argv[2];
+
+	int endid = 42; // ask josh--am i supposed to get the number of files for this?
+	hashtable_t *index_p = index_documents(endid, pgdir);
+
+	if (indexsave(index_p, idfl) != 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	hclose(index_p);
+	exit(EXIT_SUCCESS);
 }
