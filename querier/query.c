8b29f25285d45895711d6d933b28cce8e79e7963
vvvey <vuthyvey25@gmail.com>
2024-11-12 01:09:20 -0500

    Query Quiet Mode

--- querier/query.c
+++ querier/query.c
@@ -1,14 +1,3 @@
-/* 
- * query.c ---
- * 
- * Author: Samuel R. Hirsh & Vuthy Vey
- * Created: 11-07-2024
- * Version: 1.0
- * 
- * Description: 
- * 
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -17,7 +6,6 @@
 #include <queue.h>
 #include <pageio.h>
 #include <webpage.h>
-
 
 typedef struct {
   queue_t *doc_queue; // queue of documents containing the word
@@ -36,8 +24,10 @@ typedef struct {
 } rank_t;
 
 // Global Variables
-queue_t* rank_queue = NULL;
-char *pageDirectory = NULL;
+queue_t* rank_queue;
+char *pageDirectory;
+int quiet_mode = 0;
+FILE *outFile = NULL; // Global file pointer for output
 
 /*
 * @params sp, a string
@@ -67,191 +57,225 @@ bool compareDocIDRankQueue(rank_t *rp, int *doc_id) {
 }
 
 void freeIndex(void *ip) {
-	index_t* index = (index_t*)ip;
-	qapply(index->doc_queue, free);
-	qclose(index->doc_queue);
-	free(index->word);
-	free(index);
+    index_t* index = (index_t*)ip;
+    qapply(index->doc_queue, free);
+    qclose(index->doc_queue);
+    free(index->word);
+    free(index);
 }
 
 void freeRank(void *rp) {
-	rank_t* rank_p = (rank_t*)rp;
-	free(rank_p->url);
-	free(rank_p);
+    rank_t* rank_p = (rank_t*)rp;
+    free(rank_p);
 }
 
 void calcAndRank(void *docp) {
-	doc_t *dp = (doc_t*)docp;
-	
-	int count = dp->count;
-	int doc_id = dp->doc_id;
+    doc_t *dp = (doc_t*)docp;
+    int count = dp->count;
+    int doc_id = dp->doc_id;
 
-	rank_t *rp = (rank_t*)qsearch(rank_queue, (bool (*)(void *, const void *))compareDocIDRankQueue, &doc_id);
-	if (rp == NULL) {
-		rp = (rank_t*)malloc(sizeof(rank_t));
-		if (rp == NULL) {
-			fprintf(stderr, "Memory allocation failed for rank_t\n");
-			return;
-		}
-		rp->doc_id = doc_id;
-		rp->rank = count;
+    rank_t *rp = (rank_t*)qsearch(rank_queue, (bool (*)(void *, const void *))compareDocIDRankQueue, &doc_id);
+    if (rp == NULL) {
+        rp = (rank_t*)malloc(sizeof(rank_t));
+        rp->doc_id = doc_id;
+        rp->rank = count;
 
-		webpage_t *wp = pageload(doc_id, pageDirectory);
-		if (wp == NULL) {
-			fprintf(stderr, "Error: Could not load page with doc_id %d\n", doc_id);
-			free(rp); 
-			return;
-		}
-		char* url = webpage_getURL(wp);
-		rp->url = strdup(url);
-		webpage_delete(wp); 
+        webpage_t *wp = pageload(doc_id, pageDirectory);
+        if (wp == NULL) {
+            printf("Page could not be fetched\n");
+        }
+        char* url = webpage_getURL(wp);
+        
+        char* copyURL = malloc(strlen(url)*sizeof(char*));
+        strcpy(copyURL, url);
+        rp->url = copyURL;
+        
+        webpage_delete(wp);
 
-		if (rp->url == NULL) {
-			fprintf(stderr, "Memory allocation failed for URL\n");
-			free(rp); 
-			return;
-		}
-
-		qput(rank_queue, rp);
-	} else {
-		if (rp->rank > count) {
-			rp->rank = count;
-		}
-	}
+        qput(rank_queue, rp);
+    } else {
+        if (rp->rank > count) {
+            rp->rank = count;
+        }
+    }
 }
 
 void printRank(rank_t *rp) {
-	printf("rank: %d doc: %d url: %s\n", rp->rank, rp->doc_id, rp->url);
+    printf("rank: %d doc: %d url: %s\n", rp->rank, rp->doc_id, rp->url);
 }
 
-void usage(char *prog_name) {
-	printf("Usage: %s <pageDirectory> <indexFile> [-q]\n", prog_name);
+void saveRank(rank_t *rp) {
+    if (outFile != NULL) {
+        fprintf(outFile, "rank: %d doc: %d url: %s\n", rp->rank, rp->doc_id, rp->url);
+    }
+}
+
+bool compareRankID(rank_t *rp, char *strid) {
+    char doc_idstr[100];
+    sprintf(doc_idstr, "%d", rp->doc_id);
+    return strcmp(doc_idstr, strid) == 0;
+}
+
+void usage() {
+    printf("usage: query pageDirectory indexFile [-q file]\n");
+}
+
+void processQuery(char *query, hashtable_t *indexer_p) {
+    rank_queue = qopen();
+    char *delimiter = " \t";
+    char *word = strtok(query, delimiter);
+    char last_word[1024];
+    int valid = 1;
+
+    queue_t *total_parsed = qopen();
+    queue_t *connected_words = qopen();
+
+    while (word != NULL) {
+        if (NormalizeWord(word) == -1) {
+            valid = 0;
+            break;
+        }
+        if ((strcmp(last_word, "and") == 0 || strcmp(last_word, "or") == 0) &&
+            (strcmp(word, "and") == 0 || strcmp(word, "or") == 0)) {
+            valid = 0;
+            break;
+        }
+        if (strcmp(word, "or") == 0) {
+            qput(total_parsed, connected_words);
+            strcpy(last_word, word);
+            word = strtok(NULL, delimiter);
+            connected_words = qopen();
+            continue;
+        }
+        if (strcmp(word, "and") == 0) {
+            strcpy(last_word, word);
+            word = strtok(NULL, delimiter);
+            continue;
+        }
+        qput(connected_words, word);
+        strcpy(last_word, word);
+        word = strtok(NULL, delimiter);
+    }
+    if ((strcmp(last_word, "and") == 0 || strcmp(last_word, "or") == 0)) {
+        valid = 0;
+    }
+    qput(total_parsed, connected_words);
+
+    if (valid == 0) {
+        printf("Invalid query!\n");
+        qclose(rank_queue);
+        return;
+    }
+
+    queue_t *andwords = qget(total_parsed);
+    queue_t *or_queue = qopen();
+
+    rank_queue = qopen();
+
+    while (andwords != NULL) {
+        char *word = qget(andwords);
+        int allWordsPass = 1;
+        while (word != NULL) {
+            index_t *ip = hsearch(indexer_p, (bool (*)(void*, const void*))compareWord, word, strlen(word));
+            if (ip == NULL) {
+                allWordsPass = 0;
+                break;
+            } else {
+                queue_t *qp = ip->doc_queue;
+                qapply(qp, calcAndRank); 
+            }
+            word = qget(andwords);
+        }
+        if (allWordsPass == 1) {
+            qput(or_queue, rank_queue);
+            rank_queue = qopen();
+        }
+        andwords = qget(total_parsed);
+    }
+
+    hashtable_t *rank_result = hopen(100);
+
+    queue_t *tmp = qget(or_queue);
+    while (tmp != NULL) {
+        rank_t *rp = qget(tmp);
+        while (rp != NULL) {
+            int doc_id = rp->doc_id;
+            int rank = rp->rank;
+
+            char doc_idstr[100];
+            sprintf(doc_idstr, "%d", doc_id);
+
+            rank_t *r_tmp = hsearch(rank_result, (bool (*)(void*, const void*))compareRankID, doc_idstr, strlen(doc_idstr));
+            if (r_tmp == NULL) {
+                hput(rank_result, rp, doc_idstr, strlen(doc_idstr));
+            } else {
+                r_tmp->rank = r_tmp->rank + rank;
+            }
+            rp = qget(tmp);
+        }
+        tmp = qget(or_queue);
+    }
+
+    if (quiet_mode) {
+        happly(rank_result, saveRank);
+        fprintf(outFile, "\n");
+    } else {
+        happly(rank_result, printRank);
+    }
+
 }
 
 int main(int argc, char *argv[]) {
- 	if (argc < 3 || argc > 4) {
-  		usage(argv[0]);
-  		return -1;
- 	}
+    if (argc < 3 || argc > 6) {
+        usage();
+        return -1;
+    }
 
- 	int quiet_mode = 0;
- 	if (argc == 4 && strcmp(argv[3], "-q") == 0) {
- 		quiet_mode = 1;
- 	}
+    FILE *query_file = NULL;
 
-  	pageDirectory = argv[1];
-  	char* indexFile = argv[2];
+    if (argc == 6 && strcmp(argv[3], "-q") == 0) {
+        quiet_mode = 1;
+        query_file = fopen(argv[4], "r");
+        outFile = fopen(argv[5], "w");
+        if (query_file == NULL || outFile == NULL) {
+            printf("Error: Unable to open query or output file %s.\n", argv[4]);
+            return -1;
+        }
+    }
 
-  	if (access(pageDirectory, F_OK) != 0) {
-    	fprintf(stderr, "Error: %s does not exist.\n", pageDirectory);
-    	return -1;
-  	}
-	
-	hashtable_t *indexer_p = indexload(indexFile);
-	if (indexer_p == NULL) {
-		fprintf(stderr, "Can't load indexer\n");
-		return -1;
-	}
-	
-	while(1) {
-		rank_queue = qopen();
-		char query[1024];
+    pageDirectory = argv[1];
+    char *indexFile = argv[2];
+    
+    if (access(pageDirectory, F_OK) != 0) {
+        printf("Error: %s does not exist.\n", pageDirectory);
+        return -1;
+    }
+    
+    hashtable_t *indexer_p = indexload(indexFile);
 
-		printf("> ");
-		if (scanf(" %[^\n]", query) == EOF) { break; }
-		
-		char *delimiter = " \t";
-		char *word = strtok(query, delimiter);
-		char last_word[1024] = "";
-	
-		int valid = 1;
-		queue_t *total_parsed = qopen();
-		queue_t *connected_words = qopen();
-	
-		while (word != NULL) {
-			if (NormalizeWord(word) == -1) {
-				valid = 0;
-				break;
-			}
+    if (indexer_p == NULL) {
+        printf("Can't load indexer \n");
+        return -1;
+    }
 
-			if ((strcmp(last_word, "and") == 0 || strcmp(last_word, "or") == 0) && 
-			    (strcmp(word, "and") == 0 || strcmp(word, "or") == 0)) {
-				valid = 0;
-				break;
-			}
+    if (query_file) {
+        char query[1024];
+        while (fgets(query, sizeof(query), query_file)) {
+            query[strcspn(query, "\n")] = '\0';
+            processQuery(query, indexer_p);
+        }
+        fclose(query_file);
+        fclose(outFile); // Close outFile after all queries
+    } else {
+        while (1) {
+            printf("> ");
+            char query[1024];
+            if (scanf(" %[^\n]", query) == EOF) break;
+            processQuery(query, indexer_p);
+        }
+    }
 
-			if (strcmp(word, "or") == 0) {
-				qput(total_parsed, connected_words);
-				strcpy(last_word, word);
-				word = strtok(NULL, delimiter);
-				connected_words = qopen();
-				continue;
-			}
-
-			if (strcmp(word, "and") == 0) {
-				strcpy(last_word, word);
-				word = strtok(NULL, delimiter);				
-				continue;
-			}
-		
-			qput(connected_words, strdup(word));
-			
-			strcpy(last_word, word);
-			word = strtok(NULL, delimiter);
-		}
-
-		if ((strcmp(last_word, "and") == 0 || strcmp(last_word, "or") == 0)) {
-			valid = 0;
-		}
-
-		qput(total_parsed, connected_words);
-		
-		if (!valid) {
-			printf("Invalid query!\n");
-			qclose(rank_queue);
-			qapply(total_parsed, qclose);
-			qclose(total_parsed);
-			continue;
-		}
-
-		queue_t *andwords = qget(total_parsed);
-		queue_t *or_queue = qopen();
-
-		rank_queue = qopen();
-
-		while(andwords != NULL) {
-			char *word = qget(andwords);
-			while(word != NULL) {
-				printf("%s ", word);
-				index_t *ip = hsearch(indexer_p,  (bool (*)(void*, const void*))compareWord, word, strlen(word));
-				if (ip == NULL) {
-					printf("Not found: %s \n", word);
-				} else {
-					queue_t *qp = ip->doc_queue;
-					qapply(qp, calcAndRank);
-				}
-				free(word);
-				word = qget(andwords);
-			}
-			qclose(andwords);
-			qput(or_queue, rank_queue);
-			rank_queue = qopen();
-			andwords = qget(total_parsed);
-		}
-
-		while(qget(or_queue) != NULL) {
-			printf("a");
-		}
-
-		qapply(rank_queue, freeRank);
-		qclose(rank_queue);
-
-		qapply(total_parsed, qclose);
-		qclose(total_parsed);
-		qclose(or_queue);
-	}
-
-	happly(indexer_p, freeIndex);
-	hclose(indexer_p);
-	return 0;
+    happly(indexer_p, freeIndex);
+    hclose(indexer_p);
+    return 0;
 }
