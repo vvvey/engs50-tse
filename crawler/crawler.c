bf8ac0ed09c6d5ac7e53eb04997cba21798f0667
vvvey <vuthyvey25@gmail.com>
2024-11-18 17:03:31 -0500

    Stable version of crawler with thread. It could run faster

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -22,16 +22,19 @@
 #include <pageio.h>
 #include <pthread.h>
 #include <lqueue.h>
+#include <lhash.h>
 
 pthread_mutex_t fmutex;
 int file_id = 1; // global variable
 
 pthread_mutex_t pmutex;
-bool page_retrieved = true;
+bool page_retrieving = false;
+int num_page_retrieved = 0;
+int num_page_saved = 0;
 
 typedef struct {
     lqueue_t *wp_qp;
-    hashtable_t *wp_hp;
+    lhashtable_t *wp_hp;
     int maxdepth;
     char *pagedir;
 	int threadid;
@@ -52,14 +55,14 @@ bool compareURL(webpage_t *wp, char* url) {
 
 void* crawlWebpage(crawl_arg_t *cr) {
 	lqueue_t *wp_qp = cr->wp_qp;
-    hashtable_t *wp_hp = cr->wp_hp;
+    lhashtable_t *wp_hp = cr->wp_hp;
     int maxdepth = cr->maxdepth;
     char *pagedir = cr->pagedir;
 	int threadid = cr->threadid;
 
 	webpage_t *currp = (webpage_t*)lqget(wp_qp);
 
-	while (currp != NULL || page_retrieved == false) {
+	while (currp != NULL || page_retrieving == true) {
 		if (currp == NULL) {
 			currp = lqget(wp_qp);
 			continue;
@@ -74,26 +77,28 @@ void* crawlWebpage(crawl_arg_t *cr) {
 
 		// Save page
 		printf("PID%d: Saving: %s \n", threadid, webpage_getURL(currp));
-		pagesave(currp, file_id, pagedir);
+		pthread_mutex_lock(&fmutex);
+		int local_file_id = file_id;
+		file_id = file_id + 1;
+		pthread_mutex_unlock(&fmutex);
+
+		pagesave(currp, local_file_id, pagedir);
+		
+		
 		int currdepth = webpage_getDepth(currp);
 
 		int pos = 0;
 		char *result;
 
-
-		pthread_mutex_lock(&pmutex);
-
-        // Critical section
-        page_retrieved = false;
- 
+        page_retrieving = true;
 		while(currdepth < maxdepth && (pos = webpage_getNextURL(currp, pos, &result)) > 0) {
 			if (IsInternalURL(result)) {
 
 				webpage_t *wp = webpage_new(result, currdepth + 1, NULL);
 
-				if (hsearch(wp_hp, (bool (*)(void*, const void*))compareURL, result, strlen(result)) == NULL) {
+				if (lhsearch(wp_hp, (bool (*)(void*, const void*))compareURL, result, strlen(result)) == NULL) {
 					lqput(wp_qp, wp);
-					hput(wp_hp, wp, result, strlen(result));
+					lhput(wp_hp, wp, result, strlen(result));				
 				} else {
 					webpage_delete(wp);
 				}
@@ -101,22 +106,11 @@ void* crawlWebpage(crawl_arg_t *cr) {
 
 			free(result);
 		}	
-		page_retrieved = true; // might need mutex
-		// Unlock the mutex after accessing the global variable
-        pthread_mutex_unlock(&pmutex);
-
-		pthread_mutex_lock(&fmutex);
-
-        // Critical section
-        file_id = file_id + 1;
-
-        // Unlock the mutex after accessing the global variable
-        pthread_mutex_unlock(&fmutex);
-
-		
+		page_retrieving = false; // might need mutex
 		currp = lqget(wp_qp);
 	}
 	webpage_delete(currp);
+	free(cr);
 
 	return NULL;
 }
@@ -124,8 +118,8 @@ void* crawlWebpage(crawl_arg_t *cr) {
 
 int  main(int argc, char *argv[]) {
 	// Check arguments
-	if (argc != 4) {
-		printf("Usage: crawler <seedurl> <pagedir> <maxdepth> \n ");
+	if (argc != 5) {
+		printf("Usage: crawler <seedurl> <pagedir> <maxdepth> <numpthread>\n ");
 		exit(EXIT_FAILURE);
 	}
 
@@ -133,68 +127,58 @@ int  main(int argc, char *argv[]) {
 	char *seedurl = argv[1];
 	char *pagedir = argv[2];
 	int maxdepth = strtod(argv[3], &ep);
-
-	int num_threads = 4;
+	int num_threads = strtod(argv[4], &ep);
 	
-	printf("%s %s %d \n", seedurl, pagedir, maxdepth);
+	printf("%s %s %d %d\n", seedurl, pagedir, maxdepth, num_threads);
 
 	// Check if page dir exists
 	struct stat sb;
 
     if (stat(pagedir, &sb) != 0) {
 		printf("%s directory doesn't exist \n", pagedir);
-        printf("Usage: crawler <seedurl> <pagedir> <maxdepth> \n ");
+        printf("Usage: crawler <seedurl> <pagedir> <maxdepth> <numpthread>\n  ");
 		exit(EXIT_FAILURE);
     } 
 
 	// check if maxdepth is positive
 	if (strcmp(ep, "") != 0 || maxdepth < 0) {
-		printf("Usage: crawler <seedurl> <pagedir> <maxdepth> \n ");
+		printf("Usage: crawler <seedurl> <pagedir> <maxdepth> <numpthread>\n ");
 		exit(EXIT_FAILURE);
 	}
 
 	//char url[] = "https://thayer.github.io/engs50/"; 
 	int depth = 0;
 	webpage_t* homepage_p = webpage_new(seedurl, depth, NULL);
-	webpage_t* wiki_p = webpage_new("https://en.wikipedia.org/wiki/Dartmouth_College", 0, NULL);
 
 	lqueue_t *wp_qp = lqopen();
-	hashtable_t *wp_hp = hopen(100);
-	file_id = 1; // Global variable // with Mutex
-	page_retrieved = false; // Global variable
-
-	if (pthread_mutex_init(&fmutex, NULL) != 0) {
-        perror("Failed to initialize mutex");
-        exit(EXIT_FAILURE);
-    }
-
-	if (pthread_mutex_init(&pmutex, NULL) != 0) {
-        perror("Failed to initialize mutex");
-        exit(EXIT_FAILURE);
-    }
+	lhashtable_t *wp_hp = lhopen(100);
 
 	// Add homepage to queue and hash
 	lqput(wp_qp, homepage_p);
-	lqput(wp_qp, wiki_p);
-	hput(wp_hp, homepage_p, seedurl, strlen(seedurl));
-	hput(wp_hp, wiki_p, "https://en.wikipedia.org/wiki/Dartmouth_College", strlen("https://en.wikipedia.org/wiki/Dartmouth_College"));
-	
-	// Crawl
-	crawl_arg_t *cr = (crawl_arg_t*)malloc(sizeof(crawl_arg_t));
-	cr->wp_hp = wp_hp;
-	cr->wp_qp = wp_qp;
-	cr->maxdepth = maxdepth;
-	cr->pagedir = pagedir;
-	cr->threadid = 1;
+	lhput(wp_hp, homepage_p, seedurl, strlen(seedurl));
 
-	
 
+	// Thread Stuffs
+	file_id = 1; // Global variable // with Mutex
+	page_retrieving = true; // Global variable
+
+	if (pthread_mutex_init(&fmutex, NULL) != 0) { // mutex for file_id (count)
+        perror("Failed to initialize mutex");
+        exit(EXIT_FAILURE);
+    }
+
+	if (pthread_mutex_init(&pmutex, NULL) != 0) { // mutex for page_retrieved (status to tell if page is being retrieve)
+        perror("Failed to initialize mutex");
+        exit(EXIT_FAILURE);
+    }
+	
 	pthread_t *threads = (pthread_t *)malloc(num_threads * sizeof(pthread_t));
     if (threads == NULL) {
         printf("Failed to allocate memory for threads");
         exit(EXIT_FAILURE);
     }
 
+	// Create Thread
 	for (int i = 0; i < num_threads; ++i) {
 		crawl_arg_t *cr = (crawl_arg_t*)malloc(sizeof(crawl_arg_t));
 		cr->wp_hp = wp_hp;
@@ -210,7 +194,7 @@ int  main(int argc, char *argv[]) {
         }
     }
 
-
+	// Join Threads
     for (int i = 0; i < num_threads; ++i) {
         if (pthread_join(threads[i], NULL) != 0) {
             perror("Failed to join thread");
@@ -219,18 +203,15 @@ int  main(int argc, char *argv[]) {
         }
     }
 
-	lqapply(wp_qp, webpage_delete);
-
 	pthread_mutex_destroy(&fmutex);
 	pthread_mutex_destroy(&pmutex);
 
-	happly(wp_hp, webpage_delete);
-	
+
+	// Freeing stuffs
+	lqapply(wp_qp, webpage_delete);
+	lhapply(wp_hp, webpage_delete);	
 	qclose(wp_qp);
-	hclose(wp_hp);
-	
-	//	webpage_delete(homepage_p);
+	lhclose(wp_hp);
 	
 	exit(EXIT_SUCCESS);
-
 }
